43caeb322a7d3e367b4781b115f5b7d0ed7a2be5
ronbp <ronmagpantay9513@gmail.com>
2021-03-03 10:58:38 -0800

    server.js: Added a 404 page if the url is broken

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ mongoose.connect('mongodb://localhost/Tododb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
 
 const routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
